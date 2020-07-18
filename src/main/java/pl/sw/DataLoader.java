5e4560bcaea5db6d6294f490c155cadebbc33240
Szymon <sparrov@gmail.com>
2020-07-18 11:14:41 +0200

    Add DataLoader engine

--- src/main/java/pl/sw/DataLoader.java
+++ src/main/java/pl/sw/DataLoader.java
@@ -5,10 +5,43 @@ package pl.sw;
 exception związany z nieznalezieniem pliku. Zwracajmy w tym przypadku pustą listę i wydrukujmy do konsoli komunikat o błędzie.
  */
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
-/*
+
 public class DataLoader {
-    public List<String> loadData
+
+    public static List<String> loadFile(String path) {
+        List<String> loadedRows = new ArrayList<>();
+
+        try {
+            FileReader fr = new FileReader(path);
+            BufferedReader bufferReader = new BufferedReader(fr);
+
+            String line;
+
+            while ((line = bufferReader.readLine()) != null) {
+                loadedRows.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Błąd! Nie znaleziono pliku " + path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Błąd! w dostępie do pliku " + path);
+
+        }
+        return loadedRows;
+    }
+
+
+
 }
-*/
+
